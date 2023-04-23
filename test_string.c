893aad13de089af52ddbbb81fe003695d4be567a
Arsenicpoison <arseneirankunda7@gmail.com>
2023-04-23 09:52:08 -0700

    test_string function

--- test_string.c
+++ test_string.c
@@ -1,14 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * test_string - prints a string
+ * @string: string or character to print
+ * Return: the string
+ */
 int test_string(char *string)
 {
 	int test = 0;
 	int vari = 0;
-	
-	if(string)
+
+	if (string)
 	{
-		while(string[test] != '\0')
+		while (string[test] != '\0')
 		{
 			putchar(string[test]);
 			vari += 1;

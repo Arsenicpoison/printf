9b9bb354ff23ee0434d509b83f01b364c5424c0c
Arsenicpoison <arseneirankunda7@gmail.com>
2023-04-23 09:07:28 -0700

    a function that produces output according to a format.

---
+++ _printf.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * _printf - Outputs a formatted string
+ * @format: Character string to print
+ * * Return: The number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	unsigned int count = 0;
+	unsigned int tmp = 0;
+	unsigned int n;
+
+	va_start(args, format);
+	for (; format[count] != '\0'; count++)
+	{
+		if (format[count] != '%')
+		{
+			putchar(format[count]);
+		}
+		else if (format[count + 1] == 's')
+		{
+			n = test_string(va_arg(args, int));
+			count++;
+			tmp = (n - 1);
+		}
+		else if (format[count + 1] == 'c')
+		{
+			putchar(va_arg(args, int));
+			count++;
+		}
+		else if (format[count + 1] == '%')
+		{
+			putchar('%');
+		}
+		tmp = tmp + 1;
+	}
+	return (tmp);
+}

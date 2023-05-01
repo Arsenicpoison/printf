876f4ad70bea15e1e95514906682b95d7bac4c77
Calmak <cmakangadza@outlook.com>
2023-05-01 03:01:15 -0700

    convert_numbers

--- convert_numbers.c
+++ convert_numbers.c
@@ -8,7 +8,6 @@ unsigned int convert_u(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_o(va_list args, buffer_t *output
 		unsigned char flags, int wid, int prec, unsigned char len);
-
 /**
  * convert_di - converts an argument to a signed int and
  * stores it to a buffer contained in a struct
@@ -20,18 +19,11 @@ unsigned int convert_o(va_list args, buffer_t *output
  * @output: a buffer_t struct containinga a char array
  * Return: the number of bytes stored to the buffer
  */
-
-unsigned int convert_di(va_list args, buffer_t *output, 
-		unsigned char flags, int wid, int prec, unsigned char len)
-{
-	long int d;
-	long int copy;
-	unsigned int ret = 0;
-	unsigned int count = 0;
-	char pad;
-	char space = ' ';
-	char neg = '-';
-	char plus = '+';
+unsigned int convert_di(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len){
+	long int d, copy;
+	unsigned int ret = 0, count = 0;
+	char pad, space = ' ', neg = '-', plus = '+';
 
 	if (len == LONG)
 		d = va_args(args, long int);
@@ -39,9 +31,7 @@ unsigned int convert_di(va_list args, buffer_t *output,
 		d = va_args(args, int);
 	if (len == SHORT)
 		d = (short)d;
-
-	/*Handle space flag*/
-	if (SPACE_FLAG == 1 && d >= 0)
+	if (SPACE_FLAG == 1 && d >= 0)/*Handle space flag*/
 		ret += _memcpy(output, &space, 1);
 	if (prec <= 0 && NEG_FLAG == 0) /*Handle width*/
 	{
@@ -56,28 +46,20 @@ unsigned int convert_di(va_list args, buffer_t *output,
 		count += (d < 0) ? 1 : 0;
 		count += (PLUS_FLAG == 1 && d >= 0) ? 1 : 0;
 		count += (SPACE_FLAG == 1 && d >= 0) ? 1 : 0;
-
-		/*Handle plus flag when zero flag is active*/
 		if (ZERO_FLAG == 1 && PLUS_FLAG == 1 && d >= 0)
-			ret += _memcpy(output, plus &plus, 1);
-		/*Print negative sign when zero flag is active*/
+			ret += _memcpy(output, &plus, 1);
 		if (ZERO_FLAG == 1 && d < 0)
 			ret += _memcpy(output, &neg, 1);
-
 		pad = (ZERO_FLAG == 1) ? '0' : ' ';
-		for (wid -+ count; wid > 0; wid--)
+		for (wid -= count; wid > 0; wid--)
 			ret += _memcpy(output, &pad, 1);
 	}
-
-	/*Print neg sign when zero flag is not active*/
-	if (ZERO_FLAG == 0 && d < 0)
+	if (ZERO_FLAG == 0 && d < 0)/*Print neg sign when zero flag isn't active*/
 		ret += _memcpy(output, &neg, 1);
-	/*Handle plus flag when zero flag is not active*/
 	if (ZERO_FLAG == 0 && (PLUS_FLAGS == 1 && d >= 0))
 		ret += _memcpy(output, &plus, 1);
 	if (!(d == 0 && prec == 0))
-		ret += convert_sbase(output, d, "0123456789",
-				flags, 0, prec);
+		ret += convert_sbase(output, d, "0123456789", flags, 0, prec);
 	ret += print_neg_width(output, ret, flags, wid);
 	return (ret);
 }
@@ -93,8 +75,7 @@ unsigned int convert_di(va_list args, buffer_t *output,
  * @output: a buffer_t struct containinga a char array
  * Return: the number of bytes stored to the buffer
  */
-
-unsigned int convert_b(va_list args, buffer_t, *output,
+unsigned int convert_b(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len)
 {
 	unsigned int num;
@@ -117,7 +98,6 @@ unsigned int convert_b(va_list args, buffer_t, *output,
  * @output: a buffer_t struct containinga a char array
  * Return: the number of bytes stored to the buffer
  */
-
 unsigned int convert_o(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len)
 {
